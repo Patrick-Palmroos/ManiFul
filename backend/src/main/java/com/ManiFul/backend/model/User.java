6699c9e52fe2d00b9defaa63b76f67aa7dda180a
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-03 19:27:41 +0300

    Add: test user class.

--- backend/src/main/java/com/ManiFul/backend/model/User.java
+++ backend/src/main/java/com/ManiFul/backend/model/User.java
@@ -1,15 +1,25 @@
 package com.ManiFul.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@Data
+@Table(name = "User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
     private int id;
     private String username;
     private String email;
