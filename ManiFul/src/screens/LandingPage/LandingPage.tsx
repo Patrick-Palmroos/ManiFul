90e15e22956dd8fb301b0a007e121767f3e69eba
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-11 02:49:33 +0300

    Add: gradient to landing page bg.

--- ManiFul/src/screens/LandingPage/LandingPage.tsx
+++ ManiFul/src/screens/LandingPage/LandingPage.tsx
@@ -4,16 +4,25 @@ import { View, Text, Button } from 'react-native';
 import styles from './styles';
 import { LandingPageNavigationProp } from '../../types/navigation';
 import { useNavigation } from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
+import Colors from '../../styles/Colors';
 
 const LandingPage = () => {
-    const navigation = useNavigation<LandingPageNavigationProp>();
+  const navigation = useNavigation<LandingPageNavigationProp>();
 
-    return (
-        <View style={styles.container}>
-            <Text>Landing Page</Text>
-            <Button title='go to login' onPress={() => navigation.navigate("login")}/>
-        </View>
-    );
+  return (
+    <LinearGradient
+      colors={[Colors.background, Colors.backgroundWarm]}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.container}>
+      <Text>Landing Page</Text>
+      <Button
+        title="go to login"
+        onPress={() => navigation.navigate('login')}
+      />
+    </LinearGradient>
+  );
 };
 
 export default LandingPage;
