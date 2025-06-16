7f60919d2393f536dffda9cb917c4fafb4a5c766
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-16 15:51:12 +0300

    Change: validate password error message.

--- ManiFul/src/utils/validation.ts
+++ ManiFul/src/utils/validation.ts
@@ -11,5 +11,5 @@ export const validatePassword = (password: string): validationType => {
   if (password !== null && password.length >= 3)
     return { status: true, message: 'Success' };
 
-  return { status: false, message: 'An email must be provided' };
+  return { status: false, message: 'A password must be provided' };
 };
