3c2583852c47797c5d53be4afad47d48bfb80fd0
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-20 12:09:58 +0300

    Remove: old code.

--- ManiFul/App.tsx
+++ ManiFul/App.tsx
@@ -1,17 +1,8 @@
 //App.tsx
 import React from 'react';
 
-import LoginPage from './src/screens/LoginPage';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootStackParamList } from './src/types/navigation';
-import { enableScreens } from 'react-native-screens';
-import HomePage from './src/screens/HomePage';
 import { AuthProvider } from './src/context/AuthContext';
 import RootNavigation from './src/navigation/RootNavigation';
-
-enableScreens();
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
 
 import {
   Button,
@@ -22,7 +13,6 @@ import {
 } from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { NavigationContainer } from '@react-navigation/native';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
