82da579227931321831bcdd2c761503d6143a0eb
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-20 13:34:24 +0300

    Add: other pages to Tab.navigator.

--- ManiFul/src/navigation/MainStack.tsx
+++ ManiFul/src/navigation/MainStack.tsx
@@ -2,6 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MainStackParamList } from '../types/navigation';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../screens/HomePage';
+import HistoryPage from '../screens/HistoryPage';
+import ChartsPage from '../screens/ChartsPage';
+import BudgetsPage from '../screens/BudgetsPage';
 import MaterialIcons from '@react-native-vector-icons/material-icons';
 
 const Stack = createNativeStackNavigator<MainStackParamList>();
@@ -12,6 +15,21 @@ const TabNav = () => {
     <Tab.Navigator
       initialRouteName="home"
       screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="history"
+        component={HistoryPage}
+        options={{ title: 'history' }}
+      />
+      <Tab.Screen
+        name="budgets"
+        component={BudgetsPage}
+        options={{ title: 'budgets' }}
+      />
+      <Tab.Screen
+        name="charts"
+        component={ChartsPage}
+        options={{ title: 'charts' }}
+      />
       <Tab.Screen
         name="home"
         component={HomePage}
