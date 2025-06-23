67eb7a25d43c3f02228a4e372e14592ee63b6449
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-23 17:09:48 +0300

    Change: uses @RequestParam and not path variable now.

--- backend/src/main/java/com/ManiFul/backend/controller/TransactionController.java
+++ backend/src/main/java/com/ManiFul/backend/controller/TransactionController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,8 +21,8 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @GetMapping("/{id}")
-    public Transaction getTransactionWithItems(@PathVariable Long id) {
+    @GetMapping("/getById")
+    public Transaction getTransactionWithItems(@RequestParam Long id) {
         return transactionService.getTransactionWithItems(id);
     }
 }
