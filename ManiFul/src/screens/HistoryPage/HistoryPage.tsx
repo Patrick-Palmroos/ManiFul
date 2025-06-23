0bcf4f857035e2932f4992d7903fac5cee10f286
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-23 17:20:51 +0300

    Change: testing fetch on the HistoryPage.tsx.

--- ManiFul/src/screens/HistoryPage/HistoryPage.tsx
+++ ManiFul/src/screens/HistoryPage/HistoryPage.tsx
@@ -1,6 +1,37 @@
 import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { fetchTransactionWithid } from '../../api/userApi/transactionApi';
 
 const HistoryPage = () => {
+  const [test, setTest] = useState(null);
+
+  useEffect(() => {
+    const t = async () => {
+      const res = await fetchTransactionWithid({ id: 1 });
+      if (res) {
+        setTest(res);
+      } else {
+        console.error('ERRROR FETCHING: ', res);
+      }
+    };
+
+    t();
+  }, []);
+
+  if (!test) {
+    return (
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 50,
+        }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
       <Text>History</Text>
