acce8f7ad74dbea8da378eba338546dcafcbb7ae
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-23 16:40:04 +0300

    Add: transaction models.

---
+++ backend/src/main/java/com/ManiFul/backend/model/TransactionItem.java
@@ -0,0 +1,37 @@
+package com.ManiFul.backend.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "transaction_items")
+public class TransactionItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction_id", nullable = false)
+    private Transaction transaction;
+
+    private Long typeId;
+
+    private String name;
+
+    private BigDecimal total;
+}
