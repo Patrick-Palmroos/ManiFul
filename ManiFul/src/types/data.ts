34939cb4a9cf3731003ac437ecefc6ca999d3bba
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-23 13:42:09 +0300

    Add: TransactionData type.

--- ManiFul/src/types/data.ts
+++ ManiFul/src/types/data.ts
@@ -4,3 +4,13 @@ export type PieData = {
   value: number;
   color: ColorValue | null;
 };
+
+export type TransactionData = {
+  id: number;
+  title: String;
+  data: {
+    id: number;
+    name: String;
+    total: number;
+  };
+};
