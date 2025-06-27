a700530a6c477156feff430e4576dd0f3f5ddb28
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-27 19:36:39 +0300

    Add: date formatting helper function.

--- ManiFul/src/screens/HistoryPage/components/HistoryItem/HistoryItem.tsx
+++ ManiFul/src/screens/HistoryPage/components/HistoryItem/HistoryItem.tsx
@@ -2,16 +2,18 @@ import { View, Text } from 'react-native';
 import { TransactionData, TransactionItem } from '../../../../types/data';
 import styles from './styles';
 import { Shadow } from 'react-native-shadow-2';
+import { formatDateToDDMMYYYY } from '../../helper';
 
 const HistoryItem = ({ item }: { item: TransactionData }) => {
   return (
     <Shadow
-      distance={4}
-      startColor="rgba(0, 0, 0, 0.04)"
+      distance={5}
+      startColor="rgba(0, 4, 29, 0.04)"
       offset={[0, 2]}
       stretch={true}>
       <View style={styles.container}>
         <Text>{item.total}</Text>
+        <Text>{formatDateToDDMMYYYY(item.date)}</Text>
       </View>
     </Shadow>
   );
