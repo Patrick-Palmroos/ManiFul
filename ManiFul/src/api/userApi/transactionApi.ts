038b9330d8d4a66b818332dfd8d2772be61316b2
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-06-27 18:47:31 +0300

    Fix: return type.

--- ManiFul/src/api/userApi/transactionApi.ts
+++ ManiFul/src/api/userApi/transactionApi.ts
@@ -4,34 +4,35 @@ import axios from 'axios';
 import { API_URL, API_KEY } from '@env';
 import { TransactionData } from '../../types/data';
 
-export const fetchAllUserTransactions =
-  async (): Promise<TransactionData | null> => {
-    const creds = await Keychain.getGenericPassword();
-    if (creds) {
-      const { password: token } = creds;
-      try {
-        console.log('token: ', token);
-        const response = await axios.get(`${API_URL}/transactions/getAll`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'BACKEND-API-KEY': API_KEY,
-            Accept: 'application/json',
-          },
-        });
-        console.log('fetch data: ', JSON.stringify(response.data, null, 2));
+export const fetchAllUserTransactions = async (): Promise<
+  TransactionData[] | null
+> => {
+  const creds = await Keychain.getGenericPassword();
+  if (creds) {
+    const { password: token } = creds;
+    try {
+      console.log('token: ', token);
+      const response = await axios.get(`${API_URL}/transactions/getAll`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'BACKEND-API-KEY': API_KEY,
+          Accept: 'application/json',
+        },
+      });
+      console.log('fetch data: ', JSON.stringify(response.data, null, 2));
 
-        return response.data as TransactionData;
-      } catch (error) {
-        console.log('Data fetch error:', error);
-        if (axios.isAxiosError(error) && error.response?.status === 401) {
-          // Token might be expired, force logout
-        }
-        return null;
+      return response.data as TransactionData[];
+    } catch (error) {
+      console.log('Data fetch error:', error);
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        // Token might be expired, force logout
       }
-    } else {
       return null;
     }
-  };
+  } else {
+    return null;
+  }
+};
 
 export const fetchTransactionWithid = async ({
   id,
