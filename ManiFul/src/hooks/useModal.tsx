5ac0550b819605b3ebe3beb884f45e7470bfd274
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-03 15:12:36 +0300

    Add: functionality for multiple modals at once.

--- ManiFul/src/hooks/useModal.tsx
+++ ManiFul/src/hooks/useModal.tsx
@@ -1,30 +1,37 @@
 import { useState } from 'react';
 
+interface ModalItem {
+  id: string;
+  content: React.ReactNode;
+}
+
 interface UseModalResults {
   isOpen: boolean;
   openModal: (content: React.ReactNode) => void;
-  closeModal: () => void;
-  modalContent: React.ReactNode;
+  closeModal: (id: string) => void;
+  modals: ModalItem[];
 }
 
 export default function useModal(): UseModalResults {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [modalContent, setModalContent] = useState<React.ReactNode>(null);
+  const [modals, setModals] = useState<ModalItem[]>([]);
 
   const openModal = (content: React.ReactNode) => {
-    setModalContent(content);
-    setIsOpen(true);
+    const id = Math.random().toString(36).substring(2, 9); //billions of possible combinations. Play the lottery if id collision happens.
+    setModals(prev => [...prev, { id, content }]);
+    return id; // return ID to optionally control this modal later
   };
 
-  const closeModal = () => {
-    setModalContent(null);
-    setIsOpen(false);
+  const closeModal = (id?: string) => {
+    setModals(prev => {
+      if (!id) return prev.slice(0, -1); // close top modal
+      return prev.filter(m => m.id !== id);
+    });
   };
 
   return {
-    isOpen,
     openModal,
     closeModal,
-    modalContent,
+    modals,
+    isOpen: modals.length > 0,
   };
 }
