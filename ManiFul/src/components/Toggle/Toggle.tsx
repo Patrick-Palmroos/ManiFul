35a65d89e183098fbe9dea0623f46134a674203d
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-05 15:50:56 +0300

    Add: comments.

--- ManiFul/src/components/Toggle/Toggle.tsx
+++ ManiFul/src/components/Toggle/Toggle.tsx
@@ -3,6 +3,15 @@ import styles from './styles';
 import colors from '../../styles/colors';
 import text from '../../styles/text';
 
+/**
+ * Props for the Toggle component.
+ *
+ * @interface ToggleProps
+ * @property {boolean} value - Current selected value. 'true' selects field2, 'false' selects field1.
+ * @property {(value: boolean) => void} onValueChange - Callback when the toggle is changed.
+ * @property {string} [field1] - Optional label for the first option. Default is "Option 1".
+ * @property {string} [field2] - Optional label for the second option. Default is "Option 2".
+ */
 interface ToggleProps {
   onValueChange: (value: boolean) => void;
   value: boolean;
@@ -10,6 +19,13 @@ interface ToggleProps {
   field2?: string;
 }
 
+/**
+ * A custom toggle switch that allows switching between two labeled options.
+ *
+ * @component
+ * @param {ToggleProps} options - The props for the Toggle component.
+ * @returns {JSX.Element} The rendered toggle component.
+ */
 const Toggle = (options: ToggleProps) => {
   return (
     <View style={styles.container}>
