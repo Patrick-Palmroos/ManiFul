fb89e9966113059dbcb61b7714c75a2d78dee045
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-06 04:20:29 +0300

    Add: styling to ActionModal.tsx.

--- ManiFul/src/screens/ActionModal/ActionModal.tsx
+++ ManiFul/src/screens/ActionModal/ActionModal.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   Image,
   Alert,
+  TouchableOpacity,
 } from 'react-native';
 import {
   launchCamera,
@@ -25,6 +26,11 @@ import {
   TransactionPostItem,
 } from '../../types/data';
 import Toggle from '../../components/Toggle';
+import GradientButton from '../../components/GradientButton/GradientButton';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import styles from './styles';
+import colors from '../../styles/colors';
+import text from '../../styles/text';
 
 const options: CameraOptions = {
   mediaType: 'photo' as const,
@@ -37,7 +43,8 @@ const ActionModal = () => {
   const { openModal, closeModal } = useModalContext();
   const [imageUri, setImageUri] = useState<string | null>(null);
   const [toggle, setToggle] = useState<boolean>(false);
-  const [res, setRes] = useState<ImageScanType | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [resData, setResData] = useState<ImageScanType | null>(null);
 
   const handleResponse = (response: ImagePickerResponse) => {
     if (response.didCancel) {
@@ -65,9 +72,13 @@ const ActionModal = () => {
 
   const getResults = async () => {
     if (imageUri) {
-      const res = await parseReceipt(imageUri);
-      console.log(res);
-      setRes(res);
+      setLoading(true);
+      const response = await parseReceipt(imageUri);
+      if (!response) {
+        console.log('Couldnt scan receipt');
+      }
+      setResData(response);
+      setLoading(false);
     }
   };
 
@@ -83,14 +94,19 @@ const ActionModal = () => {
     );
   };
 
+  const clearData = () => {
+    setImageUri(null);
+    setResData(null);
+  };
+
   const save = async () => {
-    if (res) {
-      const [day, month, year] = res.date.split('-');
+    if (resData) {
+      const [day, month, year] = resData.date.split('-');
       const data: transactionPost = {
-        total: res.total,
-        vendor: res.vendor,
+        total: resData.total,
+        vendor: resData.vendor,
         date: new Date(`${year}-${month}-${day}`).toISOString(),
-        items: res.items.map(i => {
+        items: resData.items.map(i => {
           return {
             typeId: 7,
             name: i.name,
@@ -104,7 +120,7 @@ const ActionModal = () => {
   };
 
   return (
-    <View style={{ alignItems: 'center' }}>
+    <View style={{ alignItems: 'center', height: '70%' }}>
       <Toggle
         value={toggle}
         onValueChange={value => setToggle(value)}
@@ -113,22 +129,65 @@ const ActionModal = () => {
         width={'70%'}
       />
       {!toggle ? (
-        <View style={{ width: '100%' }}>
-          {imageUri && (
-            <Image
-              source={{ uri: imageUri }}
-              style={{
-                width: 200,
-                height: 200,
-                marginTop: 20,
-                borderRadius: 10,
-              }}
-            />
+        <View
+          style={{
+            width: '100%',
+            alignItems: 'center',
+          }}>
+          {imageUri ? (
+            <View style={styles.receiptContainer}>
+              {1 === 1 && resData ? (
+                <View
+                  style={{
+                    width: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text>{resData.total}€</Text>
+                  <Text>{resData.date}</Text>
+                </View>
+              ) : (
+                <Image
+                  source={{ uri: imageUri }}
+                  resizeMode="cover"
+                  style={styles.imageStyle}
+                />
+              )}
+              <TouchableOpacity
+                onPress={clearData}
+                disabled={loading}
+                activeOpacity={1}
+                style={
+                  loading
+                    ? { ...styles.removeButton, backgroundColor: 'grey' }
+                    : styles.removeButton
+                }>
+                <Text style={text.regularLight}>Remove</Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View style={styles.receiptContainer}>
+              <TouchableOpacity
+                onPress={openAndroidStyleChooser}
+                style={styles.receiptButton}>
+                <MaterialIcons
+                  name={'document-scanner'}
+                  size={60}
+                  color={colors.gradient}
+                />
+              </TouchableOpacity>
+            </View>
           )}
+          <GradientButton
+            text={resData ? 'Receipt details' : 'Scan receipt'}
+            onClick={resData ? () => null : getResults}
+            loading={loading}
+            width={'60%'}
+            marginTop={30}
+          />
           <Button title="results" onPress={getResults} />
           <Button title="ping" onPress={pingRasperry} />
-          <Button title="Selector" onPress={openAndroidStyleChooser} />
-          {res && <Button title="Save receipt" onPress={save} />}
+          {resData && <Button title="Save receipt" onPress={save} />}
         </View>
       ) : (
         <View>
---
+++ ManiFul/src/screens/ActionModal/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  receiptContainer: {
+    height: 180,
+    width: '60%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  receiptButton: {
+    backgroundColor: colors.light,
+    padding: 13,
+    borderRadius: 12,
+    elevation: 10,
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+  },
+  removeButton: {
+    backgroundColor: '#ba2727',
+    width: '60%',
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    elevation: 1,
+    borderRadius: 10,
+    bottom: '5%',
+  },
+});
+
+export default styles;
