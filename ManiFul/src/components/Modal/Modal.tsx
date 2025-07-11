3bc86f0e83da4a14935c2b87982360e1284bae6c
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-11 10:27:07 +0300

    Update: modal to dismiss keyboard.

--- ManiFul/src/components/Modal/Modal.tsx
+++ ManiFul/src/components/Modal/Modal.tsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Platform,
   ScrollView,
+  Keyboard,
 } from 'react-native';
 import styles from './styles';
 import MaterialIcons from '@react-native-vector-icons/material-icons';
@@ -24,36 +25,44 @@ const Modal = ({
   disableClosing = false,
 }: Props) => {
   return (
-    <View style={styles.backdropContainer}>
-      <TouchableWithoutFeedback
-        onPress={!disableClosing ? onClose : () => null}
-        touchSoundDisabled={disableClosing}>
-        <View style={styles.backdrop} />
-      </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        if (!disableClosing) {
+          onClose();
+        }
+      }}>
+      <View style={styles.backdropContainer}>
+        <TouchableWithoutFeedback
+          onPress={!disableClosing ? onClose : () => null}
+          touchSoundDisabled={disableClosing}>
+          <View style={styles.backdrop} />
+        </TouchableWithoutFeedback>
 
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        style={styles.modalWrapper}>
-        <View>
-          {/* Close Button */}
-          {closeButton && !disableClosing && (
-            <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-              {/* You can use a text fallback or an icon */}
-              {/* <Text style={styles.closeText}>×</Text> */}
-              <MaterialIcons
-                name={'close'}
-                size={30}
-                color={'white'}
-                style={styles.closeIcon}
-              />
-            </TouchableOpacity>
-          )}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          style={styles.modalWrapper}>
+          <View>
+            {/* Close Button */}
+            {closeButton && !disableClosing && (
+              <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                {/* You can use a text fallback or an icon */}
+                {/* <Text style={styles.closeText}>×</Text> */}
+                <MaterialIcons
+                  name={'close'}
+                  size={30}
+                  color={'white'}
+                  style={styles.closeIcon}
+                />
+              </TouchableOpacity>
+            )}
 
-          {/* Modal Content */}
-          <ScrollView style={styles.modalContent}>{children}</ScrollView>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
+            {/* Modal Content */}
+            <ScrollView style={styles.modalContent}>{children}</ScrollView>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
