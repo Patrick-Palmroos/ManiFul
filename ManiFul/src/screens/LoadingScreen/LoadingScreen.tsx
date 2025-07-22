2670a2bf6379e8a299ebaeaddff6d67a6293fa8b
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-23 00:54:48 +0300

    Add: styling to loadingScreen.

--- ManiFul/src/screens/LoadingScreen/LoadingScreen.tsx
+++ ManiFul/src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,11 +1,17 @@
-import { ActivityIndicator, View } from 'react-native';
-import { Text } from 'react-native-svg';
+import { ActivityIndicator, View, Image, Text } from 'react-native';
+import colors from '../../styles/colors';
+import text from '../../styles/text';
+import styles from './styles';
 
 const LoadingScreen = () => {
   return (
-    <View>
-      <ActivityIndicator />
-      <Text>Loading...</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/maniful-logo.png')}
+        style={styles.image}
+      />
+      <ActivityIndicator size={50} color={colors.highlight} />
+      <Text style={{ ...text.subtext, marginTop: 10 }}>Loading...</Text>
     </View>
   );
 };
