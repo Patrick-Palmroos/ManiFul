f3a381a06aea86f9950d85ccc8da2e7ee6190881
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-22 23:31:20 +0300

    Change: to 300 seconds.

--- ManiFul/src/api/raspberryApi.ts
+++ ManiFul/src/api/raspberryApi.ts
@@ -44,7 +44,7 @@ export const parseReceipt = async (
           'BACKEND-API-KEY': API_KEY,
           'Content-Type': 'multipart/form-data',
         },
-        timeout: 200000,
+        timeout: 300000,
       },
     );
 
