39967e615f025584258867c5b3451abf661a49ab
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-07-24 00:17:35 +0300

    Fix: styling issues.

--- ManiFul/src/screens/LoginPage/LoginPage.tsx
+++ ManiFul/src/screens/LoginPage/LoginPage.tsx
@@ -108,116 +108,124 @@ const Loginpage = () => {
         end={{ x: 1, y: 0 }}
         style={styles.container}>
         <ScrollView
-          contentContainerStyle={styles.container}
+          contentContainerStyle={styles.scrollContainer}
           automaticallyAdjustKeyboardInsets={true}>
-          <View style={{ ...styles.box }}>
-            <Image
-              source={require('../../assets/images/maniful-logo.png')}
-              style={{
-                width: '70%',
-                height: '50%',
-                resizeMode: 'contain',
-              }}
-            />
-            <Text
-              style={{
-                fontFamily: 'Rubik-Bold',
-                fontSize: 32,
-                color: '#5C0037',
-              }}>
-              ManiFul
-            </Text>
-            <Text
-              style={{
-                fontFamily: 'Rubik-SemiBold',
-                fontSize: 16,
-                color: '#68485B',
-                textAlign: 'center',
-                width: '85%',
-              }}>
-              Knowing where your money goes made easy!
-            </Text>
-            <View style={styles.loginField}>
-              <Text
+          <TouchableWithoutFeedback
+            onPress={Keyboard.dismiss}
+            accessible={false}
+            touchSoundDisabled={true}>
+            <View style={{ ...styles.box }}>
+              <Image
+                source={require('../../assets/images/maniful-logo.png')}
                 style={{
-                  ...generalStyles.errorCode,
-                  textAlign: 'center',
-                  fontSize: 16,
-                }}>
-                {error?.find(e => e.type === 'other')?.message}
-              </Text>
-              <View style={{ marginBottom: 0, width: '100%' }}>
-                <Text style={text.regular}>Email</Text>
-                <TextInput
-                  onFocus={() => handleFocusing('email')}
-                  autoCapitalize="none"
-                  onBlur={handleBlur}
-                  placeholder="Email"
-                  placeholderTextColor={colors.subText}
-                  value={input?.email}
-                  onChangeText={text => setInput({ ...input, email: text })}
-                  style={[
-                    generalStyles.textField,
-                    focusedInput === 'email' && generalStyles.textFieldFocused,
-                    error?.some(e => e.type === 'email') &&
-                      generalStyles.textFieldError,
-                  ]}
-                />
-                <Text style={generalStyles.errorCode}>
-                  {error?.find(e => e.type === 'email')?.message}
-                </Text>
-              </View>
-
-              <View style={{ marginBottom: 10, width: '100%' }}>
-                <Text style={text.regular}>Password</Text>
-                <TextInput
-                  onFocus={() => handleFocusing('password')}
-                  autoCapitalize="none"
-                  onBlur={handleBlur}
-                  placeholder="password"
-                  placeholderTextColor={colors.subText}
-                  value={input?.password}
-                  secureTextEntry={true}
-                  onChangeText={text => setInput({ ...input, password: text })}
-                  style={[
-                    generalStyles.textField,
-                    focusedInput === 'password' &&
-                      generalStyles.textFieldFocused,
-                    error?.some(e => e.type === 'password') &&
-                      generalStyles.textFieldError,
-                  ]}
-                />
-                <Text style={generalStyles.errorCode}>
-                  {error?.find(e => e.type === 'password')?.message}
-                </Text>
-              </View>
-              <GradientButton
-                text="Login"
-                onClick={onLogin}
-                loading={loading}
-                marginTop={10}
-                width={'80%'}
+                  width: '70%',
+                  height: '30%',
+                  marginTop: 30,
+                  resizeMode: 'contain',
+                }}
               />
               <Text
                 style={{
-                  ...text.subtext,
-                  maxWidth: '70%',
-                  textAlign: 'center',
-                  marginTop: 10,
+                  fontFamily: 'Rubik-Bold',
+                  fontSize: 32,
+                  color: '#5C0037',
                 }}>
-                Don't have an account yet?{' '}
+                ManiFul
+              </Text>
+              <Text
+                style={{
+                  fontFamily: 'Rubik-SemiBold',
+                  fontSize: 16,
+                  color: '#68485B',
+                  textAlign: 'center',
+                  width: '85%',
+                }}>
+                Knowing where your money goes made easy!
+              </Text>
+              <View style={styles.loginField}>
+                <Text
+                  style={{
+                    ...generalStyles.errorCode,
+                    textAlign: 'center',
+                    fontSize: 16,
+                  }}>
+                  {error?.find(e => e.type === 'other')?.message}
+                </Text>
+                <View style={{ marginBottom: 0, width: '100%' }}>
+                  <Text style={text.regular}>Email</Text>
+                  <TextInput
+                    onFocus={() => handleFocusing('email')}
+                    autoCapitalize="none"
+                    onBlur={handleBlur}
+                    placeholder="Email"
+                    placeholderTextColor={colors.subText}
+                    value={input?.email}
+                    onChangeText={text => setInput({ ...input, email: text })}
+                    style={[
+                      generalStyles.textField,
+                      focusedInput === 'email' &&
+                        generalStyles.textFieldFocused,
+                      error?.some(e => e.type === 'email') &&
+                        generalStyles.textFieldError,
+                    ]}
+                  />
+                  <Text style={generalStyles.errorCode}>
+                    {error?.find(e => e.type === 'email')?.message}
+                  </Text>
+                </View>
+
+                <View style={{ marginBottom: 10, width: '100%' }}>
+                  <Text style={text.regular}>Password</Text>
+                  <TextInput
+                    onFocus={() => handleFocusing('password')}
+                    autoCapitalize="none"
+                    onBlur={handleBlur}
+                    placeholder="password"
+                    placeholderTextColor={colors.subText}
+                    value={input?.password}
+                    secureTextEntry={true}
+                    onChangeText={text =>
+                      setInput({ ...input, password: text })
+                    }
+                    style={[
+                      generalStyles.textField,
+                      focusedInput === 'password' &&
+                        generalStyles.textFieldFocused,
+                      error?.some(e => e.type === 'password') &&
+                        generalStyles.textFieldError,
+                    ]}
+                  />
+                  <Text style={generalStyles.errorCode}>
+                    {error?.find(e => e.type === 'password')?.message}
+                  </Text>
+                </View>
+                <GradientButton
+                  text="Login"
+                  onClick={onLogin}
+                  loading={loading}
+                  marginTop={10}
+                  width={'80%'}
+                />
                 <Text
                   style={{
                     ...text.subtext,
-                    color: '#0047A3',
-                    cursor: 'pointer',
+                    maxWidth: '70%',
+                    textAlign: 'center',
+                    marginTop: 10,
                   }}>
-                  Register here.
+                  Don't have an account yet?{' '}
+                  <Text
+                    style={{
+                      ...text.subtext,
+                      color: '#0047A3',
+                      cursor: 'pointer',
+                    }}>
+                    Register here.
+                  </Text>
                 </Text>
-              </Text>
+              </View>
             </View>
-          </View>
-          <View style={{ height: 30 }} />
+          </TouchableWithoutFeedback>
         </ScrollView>
       </LinearGradient>
     </TouchableWithoutFeedback>
