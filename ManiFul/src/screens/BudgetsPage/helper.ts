843e8268eb6454fa8d34535a7370d808bba5acbe
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-01 02:52:43 +0300

    Remove: function from helper.ts

--- ManiFul/src/screens/BudgetsPage/helper.ts
+++
@@ -1,7 +0,0 @@
-export function isCurrentMonthAndYear(month: number, year: number): boolean {
-  const now = new Date();
-  const currentMonth = now.getMonth() + 1;
-  const currentYear = now.getFullYear();
-
-  return currentMonth === month && currentYear === year;
-}
