5474774a85eb2a77ab885587fa78d1806e82dd3c
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-01 04:18:12 +0300

    Add: showing of some data.

--- ManiFul/src/screens/BudgetsPage/BudgetItem/BudgetItem.tsx
+++ ManiFul/src/screens/BudgetsPage/BudgetItem/BudgetItem.tsx
@@ -1,10 +1,60 @@
 import { Text, View } from 'react-native';
 import { BudgetType } from '../../../types/budgets';
+import { useTransactions } from '../../../context/TransactionContext';
+import { useEffect, useState } from 'react';
+import { TransactionData } from '../../../types/data';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
 const BudgetItem = ({ item }: { item: BudgetType }) => {
+  const { transactions } = useTransactions();
+  const [monthsTransactions, setMonthsTransactions] = useState<
+    TransactionData[] | null
+  >(null);
+
+  const filterTransactionsByMonth = () => {
+    const data = transactions.filter(
+      t => new Date(t.date).getMonth() + 1 === item.month,
+    );
+    if (data.length === 0) return;
+    setMonthsTransactions(data);
+  };
+
+  useEffect(() => filterTransactionsByMonth(), []);
+
   return (
-    <View>
-      <Text>Item: {item.month}</Text>
+    <View
+      style={{
+        backgroundColor: 'white',
+        margin: 5,
+        height: 70,
+        borderRadius: 10,
+        elevation: 2,
+      }}>
+      {/* Date */}
+      <Text>
+        {item.year} {months[item.month - 1]}
+      </Text>
+      {/* Months expenses */}
+      <Text>
+        {monthsTransactions
+          ? monthsTransactions.reduce((sum, item) => sum + item.total, 0)
+          : 'no expenses found'}
+        /{item.budgetTotal} €
+      </Text>
     </View>
   );
 };
