c7d0f3c723139eaf71211eefe429fff8d17b0e63
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-01 06:59:17 +0300

    Add: AddBudgetItemModal.

---
+++ ManiFul/src/screens/BudgetsPage/AddBudgetItemModal/AddBudgetItemModal.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-svg';
+
+export default function AddBudgetItemModal() {
+  return (
+    <View>
+      <Text>Add them items</Text>
+    </View>
+  );
+}
