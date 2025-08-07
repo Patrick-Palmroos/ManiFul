77b7518a794d85841745e524a564aa9cd5e49d6d
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-08 02:11:35 +0300

    Add: text to displat used and total inside speedometerchart.

--- ManiFul/src/components/SpeedometerChart/SpeedometerChart.tsx
+++ ManiFul/src/components/SpeedometerChart/SpeedometerChart.tsx
@@ -1,5 +1,5 @@
 import { View } from 'react-native';
-import Svg, { G, Path } from 'react-native-svg';
+import Svg, { G, Path, Text as SvgText, Line } from 'react-native-svg';
 import * as d3Shape from 'd3-shape';
 
 type SpeedometerChartProps = {
@@ -15,7 +15,7 @@ const SpeedometerChart = ({
   used,
   total,
   backgroundColor = '#e6e6e6',
-  fillColor = '#4CAF50',
+  fillColor = '#6fa2ffff',
 }: SpeedometerChartProps) => {
   if (total <= 0) return null;
 
@@ -56,10 +56,41 @@ const SpeedometerChart = ({
     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
       <Svg width={radius * 2} height={radius}>
         <G x={radius} y={radius}>
+          {/* Arcs */}
           {/* Background */}
           <Path d={arcGenerator(fullArc) ?? ''} fill={backgroundColor} />
           {/* Used Part */}
           <Path d={arcGenerator(usedArc) ?? ''} fill={fillColor} />
+
+          {/* SVG text */}
+          <SvgText
+            x={0}
+            y={-20}
+            fontSize={14}
+            textAnchor="middle"
+            fontFamily="Rubik-Medium"
+            fill={fillColor}>
+            {`${used}€`}
+          </SvgText>
+          {/* Divider Line */}
+          <Line
+            x1={-40}
+            y1={-16}
+            x2={40}
+            y2={-16}
+            stroke={backgroundColor}
+            strokeWidth="1.5"
+          />
+          {/* SVG text */}
+          <SvgText
+            x={0}
+            y={-3}
+            fontSize={14}
+            textAnchor="middle"
+            fontFamily="Rubik-Medium"
+            fill={fillColor}>
+            {`${total}€`}
+          </SvgText>
         </G>
       </Svg>
     </View>
