1cd594e330d0557f89f9c984903c85de9d295c49
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-15 12:09:04 +0300

    Add: styles.ts to AllocationsModal.2

---
+++ ManiFul/src/screens/BudgetsPage/AllocationsModal/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../../styles/colors';
+import text from '../../../styles/text';
+import defaultStyles from '../../../styles/styles';
+
+const styles = StyleSheet.create({});
+
+export default styles;
