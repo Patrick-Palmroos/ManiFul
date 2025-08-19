51fea9a86af9ef85af3967c0aa6a458101831265
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-19 14:57:51 +0300

    Change: background color.

--- ManiFul/src/screens/ChartsPage/ChartsPage.tsx
+++ ManiFul/src/screens/ChartsPage/ChartsPage.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native';
+import colors from '../../styles/colors';
 
 const ChartsPage = () => {
   return (
-    <View>
+    <View style={{ backgroundColor: colors.background, flex: 1 }}>
       <Text>Charts</Text>
     </View>
   );
