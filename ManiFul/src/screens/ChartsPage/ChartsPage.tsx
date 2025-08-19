5c99a6e7a48d0454235fd004312645585b11e51e
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-19 15:09:55 +0300

    Add: LineChart to ChartsPage.

--- ManiFul/src/screens/ChartsPage/ChartsPage.tsx
+++ ManiFul/src/screens/ChartsPage/ChartsPage.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native';
 import colors from '../../styles/colors';
+import LineChart from './LineChart';
 
 const ChartsPage = () => {
   return (
     <View style={{ backgroundColor: colors.background, flex: 1 }}>
-      <Text>Charts</Text>
+      <View style={{ width: '100%' }}>
+        <LineChart />
+      </View>
     </View>
   );
 };
