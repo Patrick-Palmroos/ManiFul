538c8d8a6c0d2a571e33d726134420dc3699b5c0
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-20 17:19:05 +0300

    Add: styling and edit month/year button.

--- ManiFul/src/screens/ChartsPage/ChartsPage.tsx
+++ ManiFul/src/screens/ChartsPage/ChartsPage.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import colors from '../../styles/colors';
 import LineChart from './LineChart';
 import { useTransactions } from '../../context/TransactionContext';
-import { isCurrentMonthAndYear } from '../../utils/date_handling';
+import LinearGradient from 'react-native-linear-gradient';
 import { useState } from 'react';
+import { monthToTextFormat } from '../../utils/date_handling';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import text from '../../styles/text';
 
 const ChartsPage = () => {
   const { transactions } = useTransactions();
@@ -13,6 +16,7 @@ const ChartsPage = () => {
   const [selectedYear, setSelectedYear] = useState<number>(
     new Date().getFullYear(),
   );
+  const [editDate, setEditDate] = useState<boolean>(false);
 
   const values = transactions.filter(t => {
     const d = new Date(t.date);
@@ -23,24 +27,64 @@ const ChartsPage = () => {
 
   return (
     <View style={{ backgroundColor: colors.background, flex: 1 }}>
-      <View
+      <LinearGradient
+        colors={[colors.highlight, '#5C438D']}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 0, y: 0 }}
         style={{
           width: '100%',
-          height: 250,
-          backgroundColor: 'black',
-          // padding: 5,
+          paddingBottom: 15,
+          borderBottomRightRadius: 20,
+          borderBottomLeftRadius: 20,
         }}>
-        <LineChart
-          chartKey={`${selectedMonth}-${selectedYear}`}
-          year={selectedYear}
-          month={selectedMonth}
-          data={values}
-          graphColor="#C0D7FF"
-          graphColorSecondary="#681060"
-          graphLineColor="#A8FFFE"
-          textColor="#FFFFFF"
-        />
-      </View>
+        <View
+          style={{
+            height: 50,
+            flexDirection: 'row',
+            padding: 10,
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              ...text.regularMedium,
+              color: colors.light,
+              fontSize: 18,
+            }}>
+            {monthToTextFormat(selectedMonth - 1)}
+          </Text>
+          <TouchableOpacity
+            style={{
+              height: 30,
+              width: 30,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginLeft: 5,
+            }}>
+            <MaterialIcons
+              name={editDate ? 'arrow-drop-up' : 'arrow-drop-down'}
+              size={40}
+              onPress={() => setEditDate(!editDate)}
+              color={'#ffffffff'}
+              style={{
+                textAlign: 'center',
+                position: 'absolute',
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ height: 220 }}>
+          <LineChart
+            chartKey={`${selectedMonth}-${selectedYear}`}
+            year={selectedYear}
+            month={selectedMonth}
+            data={values}
+            graphColor="#C0D7FF"
+            graphColorSecondary="#681060"
+            graphLineColor="#A8FFFE"
+            textColor="#FFFFFF"
+          />
+        </View>
+      </LinearGradient>
     </View>
   );
 };
