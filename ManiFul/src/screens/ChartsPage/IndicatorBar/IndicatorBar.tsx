a49a25222073e78c7b42f3286f211c8af39db2e2
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-20 19:51:36 +0300

    Add: props.

--- ManiFul/src/screens/ChartsPage/IndicatorBar/IndicatorBar.tsx
+++ ManiFul/src/screens/ChartsPage/IndicatorBar/IndicatorBar.tsx
@@ -2,10 +2,19 @@ import { useState } from 'react';
 import { View, Text } from 'react-native';
 import Svg, { G, Text as SvgText, Line, Circle } from 'react-native-svg';
 
-const total = 1000;
-const value = 500;
+interface IndicatorBarProps {
+  total: number;
+  value: number;
+  barColor?: string;
+  lineColor?: string;
+}
 
-const IndicatorBar = () => {
+const IndicatorBar = ({
+  total,
+  value,
+  barColor = '#89004E',
+  lineColor = '#EC2C96',
+}: IndicatorBarProps) => {
   const [size, setSize] = useState<{ height: number; width: number }>({
     height: 0,
     width: 0,
@@ -28,19 +37,14 @@ const IndicatorBar = () => {
 
   return (
     <View onLayout={onLayout} style={{ flex: 1 }}>
-      <Svg
-        width={size.width + paddingX}
-        height={size.height + paddingY}
-        style={{
-          backgroundColor: 'pink',
-        }}>
+      <Svg width={size.width + paddingX} height={size.height + paddingY}>
         {/* whole bar */}
         <Line
           x1={paddingX / 2}
           y1={size.height - paddingY / 2}
           x2={size.width + paddingX / 2}
           y2={size.height - paddingY / 2}
-          stroke={'red'}
+          stroke={barColor}
           strokeWidth="12"
           strokeLinecap="round"
         />
@@ -51,7 +55,7 @@ const IndicatorBar = () => {
             y1={size.height - paddingY / 2}
             x2={lineValue + paddingX / 2}
             y2={size.height - paddingY / 2}
-            stroke={'yellow'}
+            stroke={lineColor}
             strokeWidth="12"
             strokeLinecap="round"
           />
