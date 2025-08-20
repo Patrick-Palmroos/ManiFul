d745423dbc4cf0165f8e02a9c8ecc9330d4ffaaf
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-20 19:14:03 +0300

    Add: IndicatorBar.tsx.

---
+++ ManiFul/src/screens/ChartsPage/IndicatorBar/IndicatorBar.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const IndicatorBar = () => {
+  const hello = 'World';
+
+  return (
+    <View>
+      <Text>{hello}</Text>
+    </View>
+  );
+};
+
+export default IndicatorBar;
