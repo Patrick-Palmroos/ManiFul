6762117cdb41ddf63174e9857219cb4349591dea
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-21 18:11:39 +0300

    Add: joining items and displaying some data through IndicatorBar.

--- ManiFul/src/screens/ChartsPage/ChartsPage.tsx
+++ ManiFul/src/screens/ChartsPage/ChartsPage.tsx
@@ -3,7 +3,7 @@ import colors from '../../styles/colors';
 import LineChart from './LineChart';
 import { useTransactions } from '../../context/TransactionContext';
 import LinearGradient from 'react-native-linear-gradient';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   isCurrentMonthAndYear,
   monthToTextFormat,
@@ -12,11 +12,35 @@ import MaterialIcons from '@react-native-vector-icons/material-icons';
 import text from '../../styles/text';
 import MonthPicker from 'react-native-month-year-picker';
 import IndicatorBar from './IndicatorBar';
+import { useBudgets } from '../../context/BudgetContext';
+import { useTypes } from '../../context/TypesContext';
+import { BudgetType } from '../../types/budgets';
+
+interface BudgetCategoryTypeValues {
+  name: string;
+  id: number;
+  total: number;
+  used: number;
+  types: Types[];
+}
+
+interface Types {
+  name: string;
+  id: number;
+  total: number;
+}
 
 const ChartsPage = () => {
   const { transactions } = useTransactions();
+  const { categories } = useTypes();
+  const { budgets } = useBudgets();
   const [date, setDate] = useState<Date>(new Date());
   const [editDate, setEditDate] = useState<boolean>(false);
+  const [items, setItems] = useState<BudgetCategoryTypeValues[]>([]);
+
+  console.log('transactions: ', transactions);
+  console.log('budgets: ', budgets);
+  console.log('categories: ', categories);
 
   const values = transactions.filter(t => {
     const d = new Date(t.date);
@@ -24,6 +48,70 @@ const ChartsPage = () => {
     const year = d.getFullYear();
     return month === date.getMonth() && year === date.getFullYear();
   });
+
+  const handleJoiningItems = () => {
+    //get all categories and their types
+    const list: BudgetCategoryTypeValues[] = categories
+      .map(cat => {
+        // skip if the category isnt an expense
+        if (!cat.expense) return null;
+
+        return {
+          name: cat.name,
+          id: cat.id,
+          total: 0,
+          used: 0,
+          types: cat.types.map(type => ({
+            name: type.name,
+            id: type.id,
+            total: 0,
+          })),
+        };
+      })
+      .filter(i => i !== null);
+
+    const budget: BudgetType | undefined = budgets.find(
+      b => b.month === date.getMonth() + 1 && b.year === date.getFullYear(),
+    );
+    //check if budget even exists
+    if (budget) {
+      list.forEach(item => {
+        item.total =
+          budget.items.find(bi => bi.categoryId === item.id)?.amount || 0;
+      });
+
+      list.forEach(category => {
+        category.types.forEach(type => {
+          // for each transaction
+          type.total = values.reduce((typeSum, transaction) => {
+            // find items in transaction that match the type
+            const matchingItems =
+              transaction.items?.filter(item => item.type.id === type.id) || [];
+
+            // sum only the amounts of the matching items
+            const itemsTotal = matchingItems.reduce(
+              (sum, item) => sum + item.total,
+              0,
+            );
+
+            return typeSum + itemsTotal;
+          }, 0);
+        });
+
+        // Calculate total used for the entire category
+        category.used = category.types.reduce(
+          (sum, type) => sum + type.total,
+          0,
+        );
+      });
+    }
+    console.log(list);
+    setItems(list);
+  };
+
+  useEffect(() => {
+    handleJoiningItems();
+  }, [date]);
 
   const total = values.reduce((sum, v) => (sum += v.total), 0);
 
@@ -167,19 +255,20 @@ const ChartsPage = () => {
       <View
         style={{
           backgroundColor: 'white',
-          height: 200,
           margin: 15,
           borderRadius: 15,
           padding: 10,
         }}>
-        <View style={{ height: 50 }}>
-          <IndicatorBar
-            total={100}
-            value={99}
-            title="Test"
-            barKey={`${'test'}-${100}-${99}`}
-          />
-        </View>
+        {items.map((item, i) => (
+          <View style={{ height: 50 }} key={i}>
+            <IndicatorBar
+              total={item.total}
+              value={item.used}
+              title={item.name}
+              barKey={`${item.name + item.id}-${item.total}-${item.used}`}
+            />
+          </View>
+        ))}
       </View>
     </View>
   );
