8428a98367f01dea927d8188571aed5c83da7f3f
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-22 12:46:38 +0300

    Add: styles.ts.

---
+++ ManiFul/src/screens/ChartsPage/styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
+import text from '../../styles/text';
+
+const styles = StyleSheet.create({});
+
+export default styles;
