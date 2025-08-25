f6a2c159a448f040521a2a65129c707a779a5851
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-25 13:42:24 +0300

    Add: signup screen.

---
+++ ManiFul/src/screens/Signup/Signup.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import colors from '../../styles/colors';
+
+const Signup = () => {
+  return (
+    <View style={{ backgroundColor: colors.backgroundWarm, height: '100%' }}>
+      <Text style={{ color: 'black' }}>Hi! SINGUP TIME!</Text>
+    </View>
+  );
+};
+
+export default Signup;
