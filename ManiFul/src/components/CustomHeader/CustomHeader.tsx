3196730de4c1d9559509ea45f878b5f273944c8e
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-28 16:59:50 +0300

    Change: account logo style.

--- ManiFul/src/components/CustomHeader/CustomHeader.tsx
+++ ManiFul/src/components/CustomHeader/CustomHeader.tsx
@@ -3,6 +3,7 @@ import MaterialIcons from '@react-native-vector-icons/material-icons';
 import styles from './styles';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { MainStackParamList } from '../../types/navigation';
+import colors from '../../styles/colors';
 
 type CustomHeaderProps = {
   navigation: NativeStackNavigationProp<MainStackParamList, 'TabNav'>;
@@ -19,7 +20,11 @@ const CustomHeader: React.FC<CustomHeaderProps> = ({ navigation }) => {
       <TouchableOpacity
         onPress={() => navigation.navigate('profile')}
         style={styles.iconContainer}>
-        <MaterialIcons name="account-circle" size={45} color={'black'} />
+        <MaterialIcons
+          name="account-circle"
+          size={50}
+          color={colors.gradient}
+        />
       </TouchableOpacity>
     </View>
   );
