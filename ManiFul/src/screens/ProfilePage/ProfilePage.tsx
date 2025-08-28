2bd3008598a0374ae6f2a6738787195d445314a7
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-28 17:05:58 +0300

    Add: styling.

--- ManiFul/src/screens/ProfilePage/ProfilePage.tsx
+++ ManiFul/src/screens/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,10 @@
 import { View, Text, Button } from 'react-native';
 import { useAuth } from '../../context/AuthContext';
+import text from '../../styles/text';
+import colors from '../../styles/colors';
 
 const ProfilePage = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   const handleLogout = async (): Promise<void> => {
     try {
@@ -14,9 +16,19 @@ const ProfilePage = () => {
   };
 
   return (
-    <View>
-      <Text>Profile!</Text>
-      <Button title="Logout" onPress={handleLogout} />
+    <View
+      style={{
+        backgroundColor: colors.background,
+        height: '100%',
+        padding: 10,
+      }}>
+      <Text style={{ ...text.title }}>Hello {user?.username}!</Text>
+      <Text style={text.regular}>Email: {user?.email}</Text>
+      <View style={{ alignItems: 'center' }}>
+        <View style={{ marginTop: 40, width: '60%' }}>
+          <Button title="Logout" onPress={handleLogout} />
+        </View>
+      </View>
     </View>
   );
 };
