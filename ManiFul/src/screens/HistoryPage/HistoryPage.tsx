a16b70cacc2be9a5ff36c78f80ad2379e8a0a075
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-08-28 14:38:35 +0300

    Remove: old code.

--- ManiFul/src/screens/HistoryPage/HistoryPage.tsx
+++ ManiFul/src/screens/HistoryPage/HistoryPage.tsx
@@ -1,32 +1,11 @@
 import { View, Text, ScrollView, Button } from 'react-native';
-import { useEffect, useState } from 'react';
-import {
-  fetchTransactionWithid,
-  fetchAllUserTransactions,
-} from '../../api/transactionApi';
 import colors from '../../styles/colors';
 import HistoryItem from './components/HistoryItem/HistoryItem';
-import { TransactionData } from '../../types/data';
-import { TransactionItem } from '../../types/data';
 import { useTransactions } from '../../context/TransactionContext';
-import { useTypes } from '../../context/TypesContext';
 import text from '../../styles/text';
-//TODO: save user data into a context for easy access across app without multiple fetches
+
 const HistoryPage = () => {
-  const { transactions, refreshTransactions } = useTransactions();
-  const { types, refreshData } = useTypes();
-
-  const fetchAll = async () => {
-    console.log('fetching transactions...');
-    await refreshTransactions();
-    console.log('done');
-  };
-
-  const getTypes = async () => {
-    console.log('getting types');
-    await refreshData();
-    console.log('done: ', types);
-  };
+  const { transactions } = useTransactions();
 
   if (!transactions) {
     return (
